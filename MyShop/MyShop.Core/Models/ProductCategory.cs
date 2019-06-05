53a3c7fa087f91d64f40e16478b28151700ca84e
Ashish Solanki <36642379+asolank5@users.noreply.github.com>
2019-06-05 08:54:54 -0700

    Product Grouping Adding

--- MyShop/MyShop.Core/Models/ProductCategory.cs
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -9,6 +9,11 @@ namespace MyShop.Core.Models
     public class ProductCategory
     {
         public string Id { get; set; }
+        public string Category { get; set; }
 
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
     }
 }
